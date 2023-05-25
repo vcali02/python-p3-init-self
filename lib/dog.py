b9c251bbaf6a82bfc57c8f11bea6ebbcc9e6dbf0
valeria caliguire <caliguireval@gmail.com>
2023-05-25 11:56:14 -0600

    completed

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed = "Mutt"):
+        self.name = name
+        self.breed = breed
+
+winnie = Dog("Winnie", "Poodle")
+print(winnie.name)
+print(winnie.breed)
